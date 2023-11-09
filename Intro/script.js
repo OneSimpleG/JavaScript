9a9f13608c84527ea6fdfb700fefb91989267857
Grantas A <Grantinis@gmail.com>
2023-11-09 11:37:02 +0200

    JS Intro + Pratybos

--- Intro/script.js
+++ Intro/script.js
@@ -1,4 +1,27 @@
 "use strict";
-let sentence = 'The quick brown fox jumps over the lazy dog.';
+const sentence = 'The quick brown fox jumps over the lazy dog';
+const name = 'Petras petraitis'
+const hello = 'hello world'
+const salis = 'Lietuva'
+let num = 42
+const gap = '   Tarpas  '
 
-console.log(sentence.repeat(3))
+console.log(sentence.concat(' , ',name,' , ',salis))// 'The quick brown fox jumps over the lazy dog , Petras petraitis , Lietuva'
+console.log(salis.endsWith('a'))                    // true
+console.log(hello.indexOf('world'))                 // 6
+console.log(sentence.length)                        // 43
+console.log(sentence.match(/[A-Z]/g))               // ['T']
+console.log(hello.concat(' ').repeat(3))            // hello world hello world hello world 
+console.log(sentence.replace('dog','cat'))          // The quick brown fox jumps over the lazy cat
+console.log(sentence.search('fox'))                 // 16
+console.log(name.slice(0,6))                        // 'Petras'
+console.log(salis.split())                          // ['Lietuva']
+console.log(name.startsWith('L'))                   // false
+console.log(salis.substring(2,6))                   // 'etuva'
+console.log(name.toLowerCase())                     // 'petras petraitis'
+console.log(num.toString())                         // '42'
+console.log(hello.toUpperCase())                    // 'HELLO WORLD'
+console.log(gap.trim())                             // 'Tarpas'
+console.log(num++)                                  // 42
+console.log(num)                                    // 43
+console.log(++num)                                  // 44
---
+++ Pratybos/pratybos.js
@@ -0,0 +1,49 @@
+"use strict";
+console.log('   Uzd 1.  ')
+let num = '42'
+num = Number(num)
+console.log(num)
+console.log('   Uzd 2.1.  ')
+let plus = 10
+console.log(plus++)
+console.log(plus++)
+console.log(plus++)
+console.log('   Uzd 2.2.  ')
+let minus = 10
+console.log(--minus)
+console.log(--minus)
+console.log(--minus)
+console.log(--minus)
+console.log('   Uzd 2.3.  ')
+let numb = '32'
+console.log(numb*1)
+console.log(+numb)
+console.log(Number(numb))
+console.log('   Uzd 3.  ')
+let x = 80
+let y = x + 14
+console.log(y)
+console.log('   Uzd 4.  ')
+let xSvoris = 80
+let xUgis = 180
+let ySvoris = 60
+let yUgis = 170
+let xKmi = xSvoris/((xUgis/100)**2)
+let yKmi = ySvoris/((yUgis/100)**2)
+console.log('Petro KMI:',xKmi.toFixed(2))
+console.log('Jono KMI:',yKmi.toFixed(2))
+if (xKmi > yKmi){
+    console.log('Petro KMI yra didesnis uz Jono')
+}
+else{
+    console.log('Petro KMI yra mazesnis uz Jono')
+}
+console.log('   Uzd 5.  ')
+let tx = 15
+let ty = 20
+let tz = Math.sqrt((tx**2)+(ty**2))
+console.log('X =',tx,'Y =',ty,'Z =',tz)
+console.log('   Uzd 6.  ')
+let c = 50
+let f = ((c * 9)/5)+32
+console.log('',c,'°C =',f,'°F')
