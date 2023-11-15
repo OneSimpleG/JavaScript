c660039d180af4e92e829e140ec109cea3172bfb
Moksleivis <Moksleivis@kitm.local>
2023-11-15 10:36:19 +0200

    Pratybos2

--- Pratybos2/pratybos.js
+++ Pratybos2/pratybos.js
@@ -1,16 +1,23 @@
 "use strict";
 console.log('   Uzd. 1  ')
+
 const amz = 40
 if (amz >= 40){
     console.log('Tinkamas amzius')
 }
+//
 console.log('   Uzd. 2  ')
+
 const sk = 40
 sk%2==0 ? console.log('Lyginis') : console.log('Nelyginis')
+//
 console.log('   Uzd. 3  ')
+
 const sask = 128.36
 sask > 50 ? console.log('Uzteks apmoketi mokescius') : console.log('Neuzteks apmoketi mokescius')
+//
 console.log('   Uzd. 4  ')
+
 const vidurkis = 8
 const lankomumas = 'geras'
 if (vidurkis >= 8 && lankomumas == 'geras'){
@@ -19,21 +26,27 @@ if (vidurkis >= 8 && lankomumas == 'geras'){
 else{
     console.log('Deja, stipendija nepriklauso')
 }
+//
 console.log('   Uzd. 5  ')
+
 const sk1 = 10
 const sk2 = 9
 const sk3 = 7
 if (sk1>sk2 && sk>sk3)console.log(`${sk1} yra didesnis uz ${sk2} ir ${sk3}`)
 else if (sk2>sk1 && sk2>sk3)console.log(`${sk2} yra didesnis uz ${sk1} ir ${sk3}`)
 else if (sk3>sk1 && sk3>sk2)console.log(`${sk3} yra didesnis uz ${sk1} ir ${sk2}`)
+//
 console.log('   Uzd. 6  ')
+
 const vedes = false
 const atlyginimas = 950
 const vardas = 'Petras'
 if (vedes == false && atlyginimas >= 950)console.log('Sveikiname, jus gavote paskola!')
 else if (vedes == true && atlyginimas >= 1500)console.log('Paskola bus suteikta')
 else console.log('Kai uzdibsi daugiau, ateik, pakalbesim')
+//
 console.log('   Uzd. 7  ')
+
 const diena = '3'
 switch (diena){
     case"1":
@@ -61,7 +74,9 @@ switch (diena){
         console.log('Neteisingas nurodytas skaicius')
         break
 }
+//
 console.log('   Uzd. 8  ')
+
 const men = '10'
 switch (men){
     case"1":
@@ -104,3 +119,4 @@ switch (men){
         console.log('Neteisingas nurodytas skaicius')
         break
 }
+//
